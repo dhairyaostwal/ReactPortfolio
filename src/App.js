48248b611d04307424063efdd871b5e63139624e
Dhairya Ostwal <50984984+dhairyaostwal@users.noreply.github.com>
2021-01-16 23:57:42 +0530

    Added some text More to add

--- src/App.js
+++ src/App.js
@@ -4,8 +4,24 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Dhairya Ostwal</h1>
+      <p>Hello ...</p>
+
+      <h1>my top projects</h1>
+      <p>
+        <ol>
+          <li>Tarama-OS</li>
+          <li>picPicker</li>
+          <li>Lane Detection for Self Driving Cars</li>
+        </ol>
+      </p>
+
+      <h1>learning React</h1>
+      <h3>practice tasks</h3>
+      <p></p>
+      <h3>projects based on React + Open Source Contributions</h3>
+      <p></p>
+      {/* <h2>Start editing to see some magic happen!</h2> */}
     </div>
   );
 }
